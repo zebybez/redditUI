1a0be8c47f3a95951e9159e7d7f0dda1552b0498
zebybez <zebybez@gmail.com>
2019-04-26 00:25:28 +0200

    added redditcomponent to routermodule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {RedditComponent} from './components/reddit/reddit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'r/:name', component: RedditComponent},
+  {path: '', redirectTo: '/r/all', pathMatch: 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/components/reddit/reddit.component.ts
+++ src/app/components/reddit/reddit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-reddit',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RedditComponent implements OnInit {
 
-  constructor() { }
+  name: string;
+
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.name = this.route.snapshot.paramMap.get('name');
   }
 
 }

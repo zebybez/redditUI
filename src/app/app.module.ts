2c9535427304a00c8818d1f4f73868bac04ca553
zebybez <zebybez@gmail.com>
2019-05-06 23:27:35 +0200

    fixed important missing import of HttpClientModule in appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RedditComponent } from './components/reddit/reddit.component';
-import { AccountSmallComponent } from './components/account-small/account-small.component';
-import { PostSmallComponent } from './components/post-small/post-small.component';
-import { PostListComponent } from './components/post-list/post-list.component';
-import { PostDetailComponent } from './components/post-detail/post-detail.component';
-import { CommentListComponent } from './components/comment-list/comment-list.component';
-import { CommentComponent } from './components/comment/comment.component';
-import { CommentCreateComponent } from './components/comment-create/comment-create.component';
-import { PostCreateComponent } from './components/post-create/post-create.component';
-import { RedditCreateComponent } from './components/reddit-create/reddit-create.component';
-import { LoginComponent } from './components/login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RedditComponent} from './components/reddit/reddit.component';
+import {AccountSmallComponent} from './components/account-small/account-small.component';
+import {PostSmallComponent} from './components/post-small/post-small.component';
+import {PostListComponent} from './components/post-list/post-list.component';
+import {PostDetailComponent} from './components/post-detail/post-detail.component';
+import {CommentListComponent} from './components/comment-list/comment-list.component';
+import {CommentComponent} from './components/comment/comment.component';
+import {CommentCreateComponent} from './components/comment-create/comment-create.component';
+import {PostCreateComponent} from './components/post-create/post-create.component';
+import {RedditCreateComponent} from './components/reddit-create/reddit-create.component';
+import {LoginComponent} from './components/login/login.component';
 import {FormsModule} from '@angular/forms';
-import { MessageComponent } from './components/message/message.component';
-import { RegisterComponent } from './components/register/register.component';
+import {MessageComponent} from './components/message/message.component';
+import {RegisterComponent} from './components/register/register.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import { RegisterComponent } from './components/register/register.component';
     RegisterComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule
@@ -43,4 +45,5 @@ import { RegisterComponent } from './components/register/register.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
